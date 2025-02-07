b1f5da8808fc92eb55f9af36202aa413e822206f
kajacx <kajacx@gmail.com>
2025-02-07 11:59:24 +0100

    Initial commit

---
+++ LumaMods/Class1.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.yourname.lumaisland.firstmod", "First Luma Island Mod", "1.0.0")]
+public class FirstMod : BaseUnityPlugin
+{
+    void Awake()
+    {
+        Logger.LogInfo("Hello, Luma Island! My mod is loaded!");
+    }
+}
